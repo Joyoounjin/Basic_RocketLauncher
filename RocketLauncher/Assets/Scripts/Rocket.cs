3bf63ed8736b6810495e35a3247f5c21c5860d38
Joyoounjin <dbswls060@gmail.com>
2024-10-18 12:35:58 +0900

    add Q3 setting
    - 요구사항1 구현
    - 요구사항 2 구현

--- RocketLauncher/Assets/Scripts/Rocket.cs
+++ RocketLauncher/Assets/Scripts/Rocket.cs
@@ -1,7 +1,19 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class Rocket : MonoBehaviour
 {
+
+    [SerializeField] private Text currentScoreTxt;
+    [SerializeField] private Text HighScoreTxt;
+
+    private float currentScore = 0f;
+    private float highScore = 0f;
+    private float highestPoint = 0f;
+
+
     private Rigidbody2D _rb2d;
     private float fuel = 100f;
     
@@ -12,7 +24,12 @@ public class Rocket : MonoBehaviour
     {
         _rb2d = GetComponent<Rigidbody2D>();
     }
-    
+
+    private void Update()
+    {
+        Score();
+    }
+
     public void Shoot()
     {
 
@@ -21,6 +38,20 @@ public class Rocket : MonoBehaviour
             fuel -= 10f;
             _rb2d.AddForce(Vector2.up * SPEED, ForceMode2D.Impulse);
         }
-        
+
+    }
+
+    void Score()
+    {
+        currentScore = transform.position.y;; 
+
+        if (currentScore > highestPoint)
+        {
+            highestPoint = currentScore;
+            highScore = highestPoint;
+        }
+
+        currentScoreTxt.text = $"{currentScore.ToString("F0")} M";
+        HighScoreTxt.text = $"HIGH: {highScore.ToString("F0")} M";
     }
 }
